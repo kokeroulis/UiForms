4571c3866fa11f8793ed3ae701d4d65ad306eb75
Antonis Tsiapaliokas <antonis.tsiapaliokas@mykolab.com>
2015-11-12 10:25:55 +0200

    Add validator

--- sample/src/main/java/kokeroulis/gr/sample/MainActivity.java
+++ sample/src/main/java/kokeroulis/gr/sample/MainActivity.java
@@ -2,12 +2,55 @@ package kokeroulis.gr.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import kokeroulis.gr.uiforms.forms.DoubleForm;
+import kokeroulis.gr.uiforms.forms.IntegerForm;
+import kokeroulis.gr.uiforms.validators.DoubleValidator;
+import kokeroulis.gr.uiforms.validators.IntegerValidator;
+import kokeroulis.gr.uiforms.validators.NumberValidator;
 
 public class MainActivity extends AppCompatActivity {
+    private Toast mToast;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntegerForm intForm = (IntegerForm) findViewById(R.id.int_form);
+        DoubleForm doubleForm = (DoubleForm) findViewById(R.id.double_form);
+
+        intForm.generateValidator(new IntegerValidator(0, 50));
+        doubleForm.generateValidator(new DoubleValidator(0.0, 50.0));
+
+        intForm.setInvalidInputListener(new NumberValidator.InvalidInputListener() {
+            @Override
+            public void onInvalidInput(String source, int reasonId) {
+                showError(source, reasonId);
+            }
+        });
+
+        doubleForm.setInvalidInputListener(new NumberValidator.InvalidInputListener() {
+            @Override
+            public void onInvalidInput(String source, int reasonId) {
+                showError(source, reasonId);
+            }
+        });
+    }
+
+    private void showError(String msg, int reasonId) {
+        stopToast();
+        mToast = Toast.makeText(this, "Invalid input: " + msg + " Reason: " + reasonId, Toast.LENGTH_SHORT);
+        mToast.show();
+    }
+
+    private void stopToast() {
+        if (mToast == null) {
+            return;
+        }
+        mToast.cancel();
+        mToast = null;
     }
 }
